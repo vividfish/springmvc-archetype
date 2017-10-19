00bbaafae502d01beba4a084d6a6701644ceda89
vivid <494358856@qq.com>
2017-10-19 23:49:52 +0800

    first commit

---
+++ src/main/java/com/vivid/springmvcconfig/controller/UserController.java
@@ -0,0 +1,20 @@
+package com.vivid.springmvcconfig.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+	
+	@RequestMapping("/getUser")
+	@ResponseBody
+	public String getUser() {
+		return "admin";
+	}
+	@RequestMapping("/getPage")
+	public String getPage() {
+		return "home";
+	}
+	
+}
